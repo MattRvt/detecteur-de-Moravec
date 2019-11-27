f8eeadb3191390520b5fcfd32c18e233a62eb3f1
RIVAT Matthis <52105158+MattRvt@users.noreply.github.com>
2019-11-27 09:58:51 +0100

    changement thread pour processus, grand amelioration des performance

--- src/definitionIntensite/Intensite.py
+++ src/definitionIntensite/Intensite.py
@@ -1,27 +1,28 @@
 import time
 from concurrent.futures import thread
-from threading import Thread, RLock
-
-verrou = RLock()
+from multiprocessing import Process
 
 
-class Partie(Thread):
+class Partie(Process):
     # """Thread chargé simplement d'afficher un mot dans la console."""
 
-    def __init__(self, minY, maxY, im, h, w):
-        Thread.__init__(self)
+    def __init__(self, minY, maxY, im, h, w, i, queue):
+        Process.__init__(self)
+
+        self.queue = queue
         self.minY = minY
         self.maxY = maxY
         self.im = im
         self.h = h
         self.w = w
+        self.i = i
         self.imIntensite = [[0 for j in range(w)] for i in range(h)]
 
     def run(self):
         """Code à exécuter pendant l'exécution du thread."""
-        for y in range(self.minY,self.maxY):
+        for y in range(self.minY, self.maxY):
             for x in range(self.w):
-                rayonVoisinageStatic = 1
+                rayonVoisinageStatic = 5
                 rayonVoisinage = rayonVoisinageStatic
                 voisinageParcourue = False
                 intensitePixeles = 0
@@ -29,44 +30,48 @@ class Partie(Thread):
                 somme = 0
                 while not voisinageParcourue:
                     n = 0
-                    # si on atteint le pas bord en haut de l'immage
+                    # si on atteint le pas bord en bas de l'immage
                     if (y - rayonVoisinage >= 0):
-                        # tout la rangee du haut
+                        # px du haut
                         somme = somme + self.Intensite(self.im[y - rayonVoisinage][x][0],
                                                        self.im[y - rayonVoisinage][x][1],
                                                        self.im[y - rayonVoisinage][x][2])
                         n = n + 1
-                    if (x - rayonVoisinage >= 0):
-                        # haut gauche
-                        somme = somme + self.Intensite(self.im[y - rayonVoisinage][x - rayonVoisinage][0],
-                                                       self.im[y - rayonVoisinage][x - rayonVoisinage][1],
-                                                       self.im[y - rayonVoisinage][x - rayonVoisinage][2])
-                        n = n + 1
-                        # haut droit
-                    if (x + rayonVoisinage < self.w):
-                        somme = somme + self.Intensite(self.im[y - rayonVoisinage][x + rayonVoisinage][0],
-                                                       self.im[y - rayonVoisinage][x + rayonVoisinage][1],
-                                                       self.im[y - rayonVoisinage][x + rayonVoisinage][2])
-                        n = n + 1
-                    #   si on atteint pas la limite gauche de l'image
-                    if (x - rayonVoisinage >= 0):
-                        # tout la rangee d'en bas
-                        somme = somme + self.Intensite(self.im[y][x - rayonVoisinage][0],
-                                                       self.im[y][x - rayonVoisinage][1],
-                                                       self.im[y][x - rayonVoisinage][2])
-                        n = n + 1
-                    # bas gauche
-                    if (y - rayonVoisinage >= 0):
-                        somme = somme + self.Intensite(self.im[y - rayonVoisinage][x - rayonVoisinage][0],
-                                                       self.im[y - rayonVoisinage][x - rayonVoisinage][1],
-                                                       self.im[y - rayonVoisinage][x - rayonVoisinage][2])
-                        n = n + 1
-                        # bas droit
+                        if (x - rayonVoisinage >= 0):
+                            # haut gauche
+                            for i in range(x - rayonVoisinage, x):
+                                somme = somme + self.Intensite(self.im[y - rayonVoisinage][x - i][0],
+                                                               self.im[y - rayonVoisinage][x - i][1],
+                                                               self.im[y - rayonVoisinage][x - i][2])
+                                n = n + 1
+                            # haut droit
+                        if (x + rayonVoisinage < self.w):
+                            for i in range(x + 1, x + rayonVoisinage + 1):
+                                somme = somme + self.Intensite(self.im[y - rayonVoisinage][x + rayonVoisinage][0],
+                                                               self.im[y - rayonVoisinage][x + rayonVoisinage][1],
+                                                               self.im[y - rayonVoisinage][x + rayonVoisinage][2])
+                                n = n + 1
+                    #   si on atteint pas la limite haut de l'image
                     if (y + rayonVoisinage < self.h):
-                        somme = somme + self.Intensite(self.im[y + rayonVoisinage][x - rayonVoisinage][0],
-                                                       self.im[y + rayonVoisinage][x - rayonVoisinage][1],
-                                                       self.im[y + rayonVoisinage][x - rayonVoisinage][2])
+                        # px du bas
+                        somme = somme + self.Intensite(self.im[y + rayonVoisinage][x][0],
+                                                       self.im[y + rayonVoisinage][x][1],
+                                                       self.im[y + rayonVoisinage][x][2])
                         n = n + 1
+                        if (x - rayonVoisinage >= 0):
+                            # bas gauche
+                            for i in range(x - rayonVoisinage, x):
+                                somme = somme + self.Intensite(self.im[y + rayonVoisinage][x - rayonVoisinage][0],
+                                                               self.im[y + rayonVoisinage][x - rayonVoisinage][1],
+                                                               self.im[y + rayonVoisinage][x - rayonVoisinage][2])
+                                n = n + 1
+                            # bas droit
+                        if (x + rayonVoisinage < self.w):
+                            for i in range(x + 1, x + rayonVoisinage + 1):
+                                somme = somme + self.Intensite(self.im[y + rayonVoisinage][x + rayonVoisinage][0],
+                                                               self.im[y + rayonVoisinage][x + rayonVoisinage][1],
+                                                               self.im[y + rayonVoisinage][x + rayonVoisinage][2])
+                                n = n + 1
                     rayonVoisinage -= 1
                     voisinageParcourue = rayonVoisinage == 0
                 # pixel central
@@ -76,8 +81,13 @@ class Partie(Thread):
                 imIntensiteMoy = somme / n
                 intensite = int(
                     abs(self.Intensite(self.im[y][x][0], self.im[y][x][1], self.im[y][x][2]) - imIntensiteMoy))
-                intensite = intensite / (6 * rayonVoisinageStatic)
+                intensite = intensite ** 2
                 self.imIntensite[y][x] = intensite
+        print("thread: ", self.i, " termine")
+        rep = [0, 0]
+        rep[0] = self.imIntensite
+        rep[1] = self.i
+        self.queue.put(rep)
 
     def Intensite(self, r, g, b):
-        return r * 1 + g * 3 + b * 2
+        return int((r + g + b) / 3)
--- src/main.py
+++ src/main.py
@@ -1,48 +1,44 @@
-import psutil
 import os
+from copy import deepcopy
+from multiprocessing import Queue
+
 import imageio
 from src.definitionIntensite.Intensite import Partie
 
 
-def priority():
-    """is called at every process start"""
-    p = psutil.Process(os.getpid())
-    # set to lowest priority, this is windows only, on Unix use ps.nice(19)
-    p.nice(psutil.ABOVE_NORMAL_PRIORITY_CLASS)
-
-
-try:
-    priority()
-except:
-    print("pour de meilleur performance installez psutil sur votre systeme.")
-im = imageio.imread('chargementImage/arbre.png', format='png')
+print("debut du programme")
+im = imageio.imread('chargementImage/arbreMaison.png', format='png')
 info = im.shape
 h = info[0]
 w = info[1]
 imIntensiteMoy = [[0 for j in range(w)] for i in range(h)]
-
-# creation des partie
-partie1 = Partie(int(0 * (h / 4)), int(1 * (h / 4)), im, h, w)
-partie2 = Partie(int(1 * (h / 4)), int(2 * (h / 4)), im, h, w)
-partie3 = Partie(int(2 * (h / 4)), int(3 * (h / 4)), im, h, w)
-partie4 = Partie(int(3 * (h / 4)), int(4 * (h / 4)), im, h, w)
+queue = Queue()
+# creation des parties de limages
+nombreProcess = 12
+partie = []
+for i in range(nombreProcess):
+    process = Partie(int(i * (h / nombreProcess)), int((i + 1) * (h / nombreProcess)), im, h, w, i,queue)
+    partie.append(process)
 
 # demarage des thread
-partie1.start()
-partie2.start()
-partie3.start()
-partie4.start()
+for i in range(nombreProcess):
+    partie[i].start()
 
-# attent fin des thread  et concatenation des donnes
-imIntensite = []
-partie1.join()
-imIntensite += partie1.imIntensite[int(0 * (h / 4)):int(1 * (h / 4))]
-partie2.join()
-imIntensite += partie2.imIntensite[int(1 * (h / 4)):int(2 * (h / 4))]
-partie3.join()
-imIntensite += partie3.imIntensite[int(2 * (h / 4)):int(3 * (h / 4))]
-partie4.join()
-imIntensite += partie4.imIntensite[int(3 * (h / 4)):int(4 * (h / 4))]
+# lecture des donnes
+imIntensite = [[0] * len(im[1])]*len(im)  #le tableau a la meme taille que l'image d'origine
+
+
+imRecu = []
+for i in range(nombreProcess):
+    queueGet =  queue.get()
+    imRecu = queueGet[0]
+    numProcess = queueGet[1]
+    imIntensite[int(numProcess * (h / nombreProcess)):int((numProcess + 1) * (h / nombreProcess))]\
+        = (imRecu[int(numProcess * (h / nombreProcess)):int((numProcess + 1) * (h / nombreProcess))])
+
+#verification que les process se sont bien termine
+for i in range(nombreProcess):
+    partie[i].join()
 
 # definit le code RGB de chaque pixel
 print("ecriture de l'image ")
@@ -50,9 +46,9 @@ for y in range(h):
     for x in range(w):
         contraste = 50
         if imIntensite[y][x] > 0:
-            im[y][x][0] = (255 - imIntensite[y][x] * contraste)
-            im[y][x][1] = (255 - imIntensite[y][x] * contraste)
-            im[y][x][2] = (255 - imIntensite[y][x] * contraste)
+            im[y][x][0] = (255 - (imIntensite[y][x] * contraste))
+            im[y][x][1] = (255 - (imIntensite[y][x] * contraste))
+            im[y][x][2] = (255 - (imIntensite[y][x] * contraste))
         else:
             im[y][x][0] = 0
             im[y][x][1] = 0
